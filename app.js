6ae9173200f0332ca89eb35ea14f5c0b8f293fe1
Sahand Babali <sahand.babali@gmail.com>
2023-03-11 16:04:21 +0330

    package

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const createError = require("http-errors");
-const path = require("path");
+// const path = require("path");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const logger = require("morgan");
